9c76832f06a5904200608b81bf07a9d37bed76e7
Nikmatoron-new <lars.joakim.opas@gmail.com>
2025-09-03 14:18:56 +0200

    SODAAAAAAAAAAA 9th of september

--- code.js
+++ code.js
@@ -28,6 +28,41 @@ function problem1() {
 }
 
 
+function problem2() {
+    var counter = 0   
+    var total = 0    
 
+    let by = prompt("Hva er 2026 minus 2025?") 
+    
+    let answer = parseInt(by);
 
+    if(answer == 1) {
+        alert('Interessant!')    
+    } else {
+        alert('feil')
+    }
+}
 
+window.onload = function() {
+    const rannum = Math.random();
+    
+    if (rannum < 0.5) {
+        COIN.src = "1-krone-1996-2016-kong-harald-v-samlerhuset.png";
+        COIN.alt = "kron";
+    } else {
+        COIN.src = "1-krone-1996-2016-kong-harald-v-samlerhuset-mynt.png";
+        COIN.alt = "Mynt";
+    }
+};
+
+function COINFLIP() {
+    const rannum2 = Math.random();
+    
+    if (rannum2 < 0.5) {
+        COINFLIPimg.src = "1-krone-1996-2016-kong-harald-v-samlerhuset.png";
+        COINFLIPimg.alt = "kron";
+    } else {
+        COINFLIPimg.src = "1-krone-1996-2016-kong-harald-v-samlerhuset-mynt.png";
+        COINFLIPimg.alt = "Mynt";
+    }
+};
